8afc5993701a12ec78fda1054dadba84fe95430b
tron180 <yash.mishra@raweng.com>
2019-11-04 15:29:36 +0530

    classical structure completed with chaining in promises

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 console.log('Started....');
 let adapter = require('./arrayApdapter');
 
-let array_of_numbers = [];
+var p1 = adapter.ArrayAdapter({"id": 4, "age": 47});
 
-adapter.save(array_of_numbers, { "id": 5, "age": 39 })
-.then((object) => {
-    console.log('Successfully added..!!');
-}).catch(err => {
-    console.log('Something went wrong.', err)
-})
 
-adapter.save(array_of_numbers, { "id": 5, "age": 36 })
-.then((object) => {
-    console.log('Successfully added..!!');
-}).catch(err => {
-    console.log('Something went wrong.', err)
-})
+p1.save({ "id": 5, "age": 39 })
+	.then((object) => {
+		console.log('Successfully added..!! ' + JSON.stringify(object));
+	}).catch(err => {
+		console.log('Something went wrong.', err)
+	})
 
-adapter.find(array_of_numbers, { "age": 36 })
-.then((object) => {
-    console.log('Found on ' + JSON.stringify(object))
-}).catch(err => {
-    console.log('Data Not Found.')
-})
+	.then(() => {
+		return p1.save({ "id": 5, "age": 36 })
+	}).catch(err => {
+		console.log('Something went wrong.', err)
+	})
 
-adapter.update(array_of_numbers, {'age': 36}, {'age': 63})
-    .then((array_of_numbers) => {
-        console.log('Updated Data: ' + JSON.stringify(array_of_numbers))
-    }).catch(err => {
-        console.log('Data Not Found.')
-    })
+	.then(() => {
+		return p1.find({ "id": 5 })
+	}).catch(err => {
+		console.log('Data Not Found.', err)
+	})
 
-adapter.deleted(array_of_numbers, {'age': 45})
-    .then((array_of_numbers) => {
-        console.log('Updated Data: ' + JSON.stringify(array_of_numbers))
-    }).catch(err => {
-        console.log('Data Not Found.')
-    })
+	.then((object) => {
+		console.log('f',JSON.stringify(object))
+		return p1.update({ 'age': 36 }, { 'age': 63 })
+	}).catch(err => {
+		console.log('Data Not Found.')
+	})
+
+	.then((object) => {
+		console.log('u',JSON.stringify(object))
+		return p1.delete({ 'age': 63 })
+	}).catch(err => {
+		console.log('Data Not Found.')
+	})
+
+	.then((object) => {
+		console.log('d',JSON.stringify(object))
+	}).catch(err => {
+		console.log('Data Not Found.')
+	})
--- arrayApdapter.js
+++ arrayApdapter.js
@@ -1,66 +1,138 @@
-// Save Method
-let save = (array_of_numbers, object) => {
-    return new Promise((resolve, reject) => {
-        if (Object.keys(object).length > 0) {
-            array_of_numbers.push(object);
-            resolve(
-                object
-            );
-        } else {
-            reject('Failed to add.');
-        }
-    })
-};
+class ArrayAdapter {
+    constructor(id, age){
+        this.array_of_numbers = [];
+        this.id = id;
+        this.age = age;
+    }
 
-// Find Method
-let find = (array_of_numbers, object) => {
-    return new Promise((resolve, reject) => {
-        Object.keys(object).map((key) => {
-            for (let i = 0; i <= array_of_numbers.length; i++) { // loop for array of object
-                for (let j = 0; j <= Object.keys(object).length; j++) { // loop for particular object keys
-                    if (key === Object.keys(array_of_numbers[i])[j]) {
-                        if (array_of_numbers[i][key] == object[key]) {
-                            return resolve(array_of_numbers[i]);
+    save(object) {
+        return new Promise((resolve, reject) => {
+            if (Object.keys(object).length > 0) {
+                this.array_of_numbers.push(object);
+                resolve(
+                    object
+                );
+            } else {
+                reject('Failed to add.');
+            }
+        })
+    };
+
+    find(object){
+        return new Promise((resolve, reject) => {
+            Object.keys(object).map((key) => {
+                for (let i = 0; i <= this.array_of_numbers.length; i++) { // loop for array of object
+                    for (let j = 0; j <= Object.keys(object).length; j++) { // loop for particular object keys
+                        if (key === Object.keys(this.array_of_numbers[i])[j]) {
+                            if (this.array_of_numbers[i][key] == object[key]) {
+                                return resolve(this.array_of_numbers);
+                            }
                         }
                     }
-                }
-            } reject('Failed');
-        })
-    })
-};
-
-// Update Method
-let update = (array_of_numbers, finder_object, changed_data_object) => {
-    return new Promise((resolve, reject) => {
-        array_of_numbers.map(item => {
-            Object.keys(finder_object).map(item1 => {
-                if (item[item1] == finder_object[item1]) {
-                    item[item1] = changed_data_object[item1]
-                    return resolve(array_of_numbers);
-                }
-            })
-        });
-        reject('Failed');
-    })
-};
-
-// Delete Method
-let deleted = (array_of_numbers, deleting_object) => {
-    return new Promise((resolve, reject) => {
-        array_of_numbers.map(item => {
-            Object.keys(deleting_object).map(item1 => {
-                if(item[item1] == deleting_object[item1]) {
-                    delete item[item1]
-                    return resolve(array_of_numbers)
-                }
+                } reject('Failed');
             })
         })
-        reject('Failed');
-    })
+    };
+
+    update(finder_object, changed_data_object){
+        return new Promise((resolve, reject) => {
+            this.array_of_numbers.map(item => {
+                Object.keys(finder_object).map(item1 => {
+                    if (item[item1] == finder_object[item1]) {
+                        item[item1] = changed_data_object[item1]
+                        return resolve(this.array_of_numbers);
+                    }
+                })
+            });
+            reject('Failed');
+        })
+    };
+
+    delete(deleting_object){
+        return new Promise((resolve, reject) => {
+            this.array_of_numbers.map((item, key) => {
+                Object.keys(deleting_object).map(item1 => {
+                    if(item[item1] == deleting_object[item1]) {
+                        delete this.array_of_numbers[key]
+                        return resolve(this.array_of_numbers.filter((element) => {return element != null}))
+                    }
+                })
+            })
+            reject('Failed');
+        })
+    };
 }
 
+exports.ArrayAdapter = (id, age) => {
+    return new ArrayAdapter(id, age);
+}
+
+
+// Save Method
+// let save = (array_of_numbers, object) => {
+//     return new Promise((resolve, reject) => {
+//         if (Object.keys(object).length > 0) {
+//             array_of_numbers.push(object);
+//             resolve(
+//                 object
+//             );
+//         } else {
+//             reject('Failed to add.');
+//         }
+//     })
+// };
+
+// Find Method
+// let find = (array_of_numbers, object) => {
+//     return new Promise((resolve, reject) => {
+//         Object.keys(object).map((key) => {
+//             for (let i = 0; i <= array_of_numbers.length; i++) { // loop for array of object
+//                 for (let j = 0; j <= Object.keys(object).length; j++) { // loop for particular object keys
+//                     if (key === Object.keys(array_of_numbers[i])[j]) {
+//                         if (array_of_numbers[i][key] == object[key]) {
+//                             return resolve(array_of_numbers[i]);
+//                         }
+//                     }
+//                 }
+//             } reject('Failed');
+//         })
+//     })
+// };
+
+// Update Method
+// let update = (array_of_numbers, finder_object, changed_data_object) => {
+//     return new Promise((resolve, reject) => {
+//         array_of_numbers.map(item => {
+//             Object.keys(finder_object).map(item1 => {
+//                 if (item[item1] == finder_object[item1]) {
+//                     item[item1] = changed_data_object[item1]
+//                     return resolve(array_of_numbers);
+//                 }
+//             })
+//         });
+//         reject('Failed');
+//     })
+// };
+
+// Delete Method
+// let deleted = (array_of_numbers, deleting_object) => {
+//     return new Promise((resolve, reject) => {
+//         array_of_numbers.map(item => {
+//             Object.keys(deleting_object).map(item1 => {
+//                 if(item[item1] == deleting_object[item1]) {
+//                     delete item[item1]
+//                     return resolve(array_of_numbers)
+//                 }
+//             })
+//         })
+//         reject('Failed');
+//     })
+// }
+
+
+
 // Exports
-exports.save = save;
-exports.find = find;
-exports.update = update;
-exports.deleted = deleted;
+// exports.save = ArrayAdapter.save;
+// exports.find = ArrayAdapter.find;
+// exports.update = ArrayAdapter.update;
+// exports.deleted = ArrayAdapter.deleted;
